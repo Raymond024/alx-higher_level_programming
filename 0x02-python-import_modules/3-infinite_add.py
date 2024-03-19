466b673779809f132a9a864a5ce8e30d6e6acd19
Raymond024 <raymondmugimba2@gmail.com>
2024-03-19 08:20:27 -0700

    4

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    tot = 0
+    for z in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(tot))

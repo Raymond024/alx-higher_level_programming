7b99ea1e602be5597f07ee5fa7447c80e4be4dc4
Raymond024 <raymondmugimba2@gmail.com>
2024-06-20 03:39:43 -0700

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const s = Math.floor(Number(process.argv[2]));
 if (isNaN(s)) {
   console.log('Missing size');
 } else {
-  for (let v = 0; v < size; v++) {
+  for (let v = 0; v < s; v++) {
     let r = '';
     for (let y = 0; y < s; y++) r += 'X';
     console.log(row);
